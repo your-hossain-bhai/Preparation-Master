fdc7c693b40896f4df38152762a1b44c0ebb247d
your-hossain-bhai <mdhossainhossain747@gmail.com>
2026-09-08 22:00:18 +0600

    Add Capacitor Android support

---
+++ android/app/src/main/java/com/preparationmaster/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.preparationmaster.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.preparationmaster.app',
+  appName: 'Preparation Master',
+  webDir: 'dist'
+};
+
+export default config;
